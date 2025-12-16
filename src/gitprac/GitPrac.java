dcfea24e6fed66a97c0092daaae79b33643bba4c
Umer-3085 <l233085@lhr.nu.edu.pk>
2025-12-16 17:30:12 +0500

    main change

--- src/gitprac/GitPrac.java
+++ src/gitprac/GitPrac.java
@@ -15,6 +15,7 @@ public class GitPrac {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println();
         System.out.println("Hello oiii");
     }
     
